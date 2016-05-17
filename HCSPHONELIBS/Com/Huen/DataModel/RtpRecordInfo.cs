59a44d080c55ef59b991d254eee2af17be6f773a
JC LEE <step4u@gmail.com>
2016-05-17 19:37:27 +0900

    2016-05-17 19:37

--- HCSPHONELIBS/Com/Huen/DataModel/RtpRecordInfo.cs
+++ HCSPHONELIBS/Com/Huen/DataModel/RtpRecordInfo.cs
@@ -80,8 +80,8 @@ namespace Com.Huen.DataModel
 
         public void Add(RecordInfo_t obj)
         {
-            //string msg = string.Format("seq:{0}, ext:{1}, peer:{2}, isExtension:{3}, size:{4}, bytesLength:{5}, codec:{6}", obj.seq, obj.extension, obj.peer_number, obj.isExtension, obj.size - 12, obj.voice.Length, obj.codec);
-            //util.WriteLogTest3(msg, this.filename);
+            string msg = string.Format("seq:{0}, ext:{1}, peer:{2}, isExtension:{3}, size:{4}, bytesLength:{5}, codec:{6}", obj.seq, obj.extension, obj.peer_number, obj.isExtension, obj.size - 12, obj.voice.Length, obj.codec);
+            util.WriteLogTest3(msg, this.filename);
 
             if (obj.size == 0)
                 endcount++;
